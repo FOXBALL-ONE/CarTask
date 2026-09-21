9fc2f5a9341ad7bc14c1bb2da616f8bfc13662da
FOXBALL-ONE <admin@utf-8.fun>
2026-09-21 16:17:00 +0800

    config(keytop): 配置应用启动和运行参数
    - 完善 Keytop 应用主类
    - 添加 H2 内存数据库配置
    - 设置日志级别和服务端口 8091

--- keytop/src/main/kotlin/top/itneko/keytop/KeytopApplication.kt
+++ keytop/src/main/kotlin/top/itneko/keytop/KeytopApplication.kt
@@ -1,9 +1,11 @@
 package top.itneko.keytop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(KeytopMockProperties::class)
 class KeytopApplication
 
 fun main(args: Array<String>) {
