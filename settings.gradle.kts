b75f2c6579cf9dded0e3336b852302b709add337
FOXBALL-ONE <admin@utf-8.fun>
2026-09-22 13:20:05 +0800

    增加更多插件镜像源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://mirrors.huaweicloud.com/repository/maven/")
+        maven("https://mirrors.cloud.tencent.com/maven/")
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "carTask"
